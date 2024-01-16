e189f7459c0aacc6adb37409e2cee7c03b36e524
Amit-Cohen10 <amit.cohen04@post.runi.ac.il>
2024-01-16 19:45:39 +0200

    first

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,90 @@
 public class ArrayOps {
     public static void main(String[] args) {
-        
+        int [] array = {0,1,2,3,4,6};
+        int [] array2 = {0,1,7,3};
+        sortarray(array2);
+        for(int i=0; i<array2.length; i++){
+            System.out.print(array2[i]+ " ");
+        }
+        System.out.println(contain(array2, array));     
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        boolean foundelement =false;
+        int index =-1;
+        for(int i=0; i<array.length; i++){
+            foundelement =false;
+            index =-1;
+            for(int j=0; j<array.length; j++){
+                index++;
+                if(array[i]==j){
+                    foundelement =true;
+                }
+            }
+            if (foundelement==false) {
+                return index;
+            }
+        }
+        return array.length;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        sortarray(array);
+        return array[array.length-2];
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
+        if (contain(array1, array2)==true && contain(array2, array1)==true) {
+            return true;
+        }
         return false;
     }
+    
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
+        int n = array.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (array[j - 1] > array[j]) {
+                    //swap the elements
+                    temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
         return false;
     }
 
+    public static void sortarray(int[] array) {
+        int n = array.length;
+        int temp = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 1; j < (n - i); j++) {
+                if (array[j - 1] > array[j]) {
+                    //swap the elements
+                    temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+}
+// the function gets two arrays and checks if the array2 contains array1
+    public static boolean contain(int [] array1, int [] array2){
+        boolean contain = true;
+        for(int i=0; i<array1.length; i++){
+            contain = false;
+            for(int j=0; j<array2.length; j++){
+                if (array1[i]==array2[j]) {
+                    contain = true;
+                }
+            }
+            if (contain==false) {
+                return false;
+            }
+        }
+        return contain;
+    }
 }
--- StringOps.java
+++ StringOps.java
@@ -22,21 +22,83 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
-        
+        //System.out.println(capVowelsLowRest("amit"));
+        String cam = "HeLLO    World";
+        System.err.println(cam.length());
+        System.out.println(camelCase(cam));
+/*         int [] arr = allIndexOf("AAAA", 'A');
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        } */
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        String vowels = "aeiouAEIOU";
+        String newstring = "";
+        for(int i=0; i<string.length(); i++){
+            if(vowels.indexOf(string.charAt(i))!=-1){
+                newstring = newstring + (char)(string.charAt(i) -32);
+            }
+            else{
+                newstring = newstring + string.charAt(i);
+            }
+        }
+        return newstring;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        String newstring = "";
+        String lowerstring ="";
+        for(int i=0; i<string.length(); i++){
+            if (string.charAt(i) >= 'A' && string.charAt(i) <= 'Z') {
+                lowerstring = lowerstring + (char)(string.charAt(i) +32);
+            }
+            else{
+                lowerstring = lowerstring +string.charAt(i);
+            }
+        }
+        for(int i =0; i<lowerstring.length(); i++){
+            if (lowerstring.charAt(i)!=32) {
+                newstring = newstring + lowerstring.charAt(i);
+            }
+            else{
+                if (i == lowerstring.length() -1) {
+                    i++;
+                }
+                else{
+                    if (lowerstring.charAt(i+1) >= 'a' && lowerstring.charAt(i+1) <= 'z') {
+                        newstring = newstring +(char) (lowerstring.charAt(i+1)-32);
+                        i++;   
+                    }
+                    else{
+                        if (lowerstring.charAt(i)==32) {
+                            lowerstring = lowerstring;
+                        }
+                        else{
+                            newstring = newstring + lowerstring.charAt(i);
+                        }
+                    }
+                }
+            }
+        }
+        return newstring;
     }
 
     public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+        int size = 0;
+        int counter=0;
+        for(int i=0; i<string.length(); i++){
+            if (string.charAt(i)==chr) {
+                size++;
+            }
+        } 
+        int [] arr = new int [size];
+        for(int i =0; i<arr.length; i++){
+            if (string.charAt(i)==chr) {
+                arr[counter]=i;
+                counter++;
+            }
+        }
+        return arr;
     }
 }
